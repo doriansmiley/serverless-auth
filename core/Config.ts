d526ed6a64ceeda686d3302d59ad6921350ee31b
dorian smiley <doriansmiley@yahoo.com>
2019-04-23 10:00:57 -0700

    remove unused boiler plate

--- core/Config.ts
+++ core/Config.ts
@@ -4,10 +4,6 @@ import {StringUtils} from '../util/StringUtils';
 export class Config implements IConfig {
 
     protected _serviceMap: Object = null; // hash to look up routes
-
-    public baseUrl: string; // base http address of application ie http://localhost:3000. Note DO NOT include the trailing /.
-    public webRoot: string = ''; // path relative to webroot where the application is deployed
-    public token: string = null; // used for oAuth authentication schemes and similar token based authentication systems
     // used for assigning a concrete service implementation. Must be in the form: /v + version number. for example: /v1
     public serviceCode: string = null;
 
